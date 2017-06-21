297761616621b92006a45aabdd8884df027a013b
rsokhonn <rainy.sokhonn@gmail.com>
2017-06-21 15:56:36 +0200

    After 2nd client review

--- controller/script.js
+++ controller/script.js
@@ -1,6 +1,7 @@
 var myIndex = 0;
 carousel();
 
+/* This function implments the automated slideshow of imgaes. */
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
@@ -13,6 +14,7 @@ function carousel() {
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
 
+/* This function prompts the user for name and display a greeting on the fixed menu. */
 function login() {
 	var name = prompt("Please input your name: ");
 
@@ -20,4 +22,9 @@ function login() {
 		// document.getElementsById("login").innerHTML = "<button id=\"login-button\" style=\"visibility:hidden;\"> <b> Hello " + name + " ! </b> </button>";
 		document.getElementById("login").innerHTML = "<p style=\"position: fixed; color: white; z-index: 30; font-size: 150%; font-family: \"Palatino Linotype\", \"Book Antiqua\", Palatino, serif;\"> Hello, " + name + "! </p>";
 	}
+}
+
+/* This function changes the background of the webpage. */
+function switchbg(n){
+    document.body.style.backgroundImage = "url(\"img/bg0" + n + ".jpg\")";
 }
